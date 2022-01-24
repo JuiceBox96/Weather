27dede295b420bfa61034893e0dd099881dcd0ce
Terrez <75124951+JuiceBox96@users.noreply.github.com>
2022-01-24 13:23:23 +0000

    Update app.js

--- Weather Application/Strcuture/app.js
+++ Weather Application/Strcuture/app.js
@@ -10,7 +10,15 @@ button.addEventListener('click', function(name){
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=50a7aa80fa492fa92e874d23ad061374')
 .then(response => response.json())
 .then(data => {
-})
+  
+  let tempValue = data['main']['temp'];
+  let nameValue = data['name'];
+  let descValue = data['weather'][0]['description'];
 
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Desc - "+descValue;
+  temp.innerHTML = "Temp - "+tempValue;
+  input.value ="";
+})
 .catch(err => alert("Wrong city name!"));
 })
